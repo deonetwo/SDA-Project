56f1c388115927bd675c14a6187208610a557ef8
Deonetwo <48423286+deonetwo@users.noreply.github.com>
2019-06-07 17:24:26 +0700

    Update main.c
    Forgot the main

--- main.c
+++ main.c
@@ -9,15 +9,53 @@
 
 #include<stdio.h>
 #include<windows.h>
+#include"LinkedList.h"
+
+#define INPUTDATA 1
+#define SHOWTREE 2
+#define SHOWCODE 3
+#define EXIT 4
 
 int main(){
 	int MenuSelect;
+	char Input[100];
+	
+	List DataHuruf;
+	
+	CreateList(&DataHuruf);
 	
 	MenuSelect = 0;
-	while(MenuSelect == 0){
+	while(MenuSelect != 4){
 		MenuSelect = MainMenu();
-	}
-}
+		
+		switch(MenuSelect){
+			case INPUTDATA:{
+				printf("Input kata/kalimat:\n"); 
+				scanf(" %[^\n]s",Input); // Input data dari keyboard
+				//Test
+				int i;
+				for(i=0;i<strlen(Input);i++){
+					printf("%c\n",Input[i]);
+				}
+				printf("finish\n");
+				//End Test
+				system("pause");
+				break;
+			} //end Case 1
+			case SHOWTREE:{
+				printf("Test select 2");
+				break;
+			} //end Case 2
+			case SHOWCODE:{
+				printf("Test s;ect 3");
+				break;
+			} //end Case 3
+			case EXIT:{
+				break;
+			} //end Case 4
+		} //end Switch
+	} //end While
+} //end Main
 
 int MainMenu(){
 	int Pilihan;
@@ -26,8 +64,9 @@ int MainMenu(){
 	printf("1. Input data berupa kalimat.\n");
 	printf("2. Tampilkan Tree.\n");
 	printf("3. Tampilkan kode huruf.\n");
+	printf("4. Exit.\n");
 	printf("Pilihan: ");scanf("%d",&Pilihan);
-	if(Pilihan > 3 || Pilihan < 1){
+	if(Pilihan > 4 || Pilihan < 1){ //Program error ketika input berupa huruf
 		printf("Pilihan tidak tersedia.\n");
 		system("pause");
 		Pilihan = 0;
